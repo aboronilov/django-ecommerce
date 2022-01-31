033eb46dc112ca9356213313cf390354367ed2f0
= <=>
2022-01-31 15:47:52 +0200

    orders rendered - OK

--- store/views.py
+++ store/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Order
 
 
 def store(request):
     products = Product.objects.all()
-    context = {
-        'products': products,
-    }
+    context = {'products': products, }
     return render(request, 'store/store.html', context=context)
 
 
 def cart(request):
-    context = {
-
-    }
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items, }
     return render(request, 'store/cart.html', context=context)
 
 
